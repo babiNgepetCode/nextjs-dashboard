e73b9cda8a226ac0d04e5c977d1edf2b25a4d757
michael.j <michael.j@hubble.build>
2024-01-17 15:24:15 +0800

    Chapter 5: Navigation

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <p>Dashboard Page</p>
+    </>
+  );
+}
